77c214cb84dfb8e950bce0fbe5fa92d49c8ac158
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2026-01-16 19:41:37 -0800

    Added Robot Vision Sim

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -93,9 +93,19 @@ public class Robot extends TimedRobot {
 
   /** This function is called once when the robot is first started up. */
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+    // Reset vision simulation pose if needed
+    if (m_robotContainer.vision != null) {
+      m_robotContainer.vision.resetSimPose(m_robotContainer.drivetrain.getState().Pose);
+    }
+  }
 
   /** This function is called periodically whilst in simulation. */
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+    // Update vision simulation with current robot pose
+    if (m_robotContainer.vision != null) {
+      m_robotContainer.vision.simulationPeriodic(m_robotContainer.drivetrain.getState().Pose);
+    }
+  }
 }
