01c09efd9678656c55b5892cc5a6ed10e0e171c8
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2026-01-26 16:48:03 -0800

    Added Camera Position on Robot

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -27,9 +27,10 @@ public class VisionConstants {
     public static String camera1Name = "camera_1";
 
     // Robot to camera transforms
-    // (Not used by Limelight, configure in web UI instead)
-    public static Transform3d robotToCamera0 = new Transform3d(0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, 0.0));
-    public static Transform3d robotToCamera1 = new Transform3d(-0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI));
+    public static Transform3d robotToCamera0 =
+        new Transform3d(0.228, 0.0381, 0.482, new Rotation3d(0.0, Math.toRadians(-22.2), 0.0));
+    public static Transform3d robotToCamera1 =
+        new Transform3d(0.0, 10.0, 0.1524, new Rotation3d(0.0, 0.0, 0.0));
 
     // Basic filtering thresholds
     public static double maxAmbiguity = 0.3;
