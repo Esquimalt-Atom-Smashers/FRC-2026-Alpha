33c312f2cd717999c7ce32506188b0950deda75f
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2026-02-15 00:47:09 -0800

    Simplified Turret to use slew-rate-limited setpoint following

--- src/main/java/frc/robot/subsystems/shooter/flywheel/FlywheelIOSim.java
+++ src/main/java/frc/robot/subsystems/shooter/flywheel/FlywheelIOSim.java
@@ -1,72 +1,30 @@
 package frc.robot.subsystems.shooter.flywheel;
 
-import static frc.robot.subsystems.shooter.flywheel.FlywheelConstants.kGearRatio;
-import static frc.robot.subsystems.shooter.flywheel.FlywheelConstants.kMaxVoltage;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.util.Units;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.simulation.DCMotorSim;
-
-/** Flywheel IO for simulation; software velocity PIDF. */
+/** Flywheel IO for simulation; slew-rate-limited setpoint following. */
 public class FlywheelIOSim implements FlywheelIO {
 
-  private static final double kLoopPeriodSecs = 0.02;
+  /** Max acceleration: 4000 RPM in 2 s (rad/s²). */
+  private static final double kMaxAccelRadPerSecSq = Units.rotationsPerMinuteToRadiansPerSecond(4000.0) / 2.0;
 
-  private final DCMotor motorModel = DCMotor.getKrakenX44Foc(1); // TODO: Add correct motor model
-  private final DCMotorSim motorSim =
-      new DCMotorSim(
-          LinearSystemId.createDCMotorSystem(motorModel, 0.001, 1.0 / kGearRatio),
-          motorModel);
+  private final SlewRateLimiter slewRateLimiter = new SlewRateLimiter(kMaxAccelRadPerSecSq);
 
   private double targetVelocityRadsPerSec = 0.0;
-  private double integralRotSec = 0.0;
-  private double previousRotPerSec = 0.0;
-  private double appliedVolts = 0.0;
   private boolean isStopped = false;
 
   @Override
   public void updateInputs(FlywheelIOInputs inputs) {
-    double p = SmartDashboard.getNumber("Flywheel/kP", FlywheelConstants.kP);
-    double i = SmartDashboard.getNumber("Flywheel/kI", FlywheelConstants.kI);
-    double d = SmartDashboard.getNumber("Flywheel/kD", FlywheelConstants.kD);
-    double v = SmartDashboard.getNumber("Flywheel/kV", FlywheelConstants.kV);
-    double s = SmartDashboard.getNumber("Flywheel/kS", FlywheelConstants.kS);
-
     if (isStopped) {
-      integralRotSec = 0.0;
-      appliedVolts = 0.0;
-      motorSim.setInputVoltage(0.0);
-      previousRotPerSec = motorSim.getAngularVelocityRadPerSec() / kGearRatio / (2.0 * Math.PI);
-    } else {
-      double currentVelocityRadsPerSec =
-          motorSim.getAngularVelocityRadPerSec() / kGearRatio;
-      double targetRotPerSec = targetVelocityRadsPerSec / (2.0 * Math.PI);
-      double currentRotPerSec = currentVelocityRadsPerSec / (2.0 * Math.PI);
-      double errorRotPerSec = targetRotPerSec - currentRotPerSec;
-
-      integralRotSec += errorRotPerSec * kLoopPeriodSecs;
-      double maxIntegralSec = i > 1e-9 ? kMaxVoltage / i : 1e6;
-      integralRotSec = MathUtil.clamp(integralRotSec, -maxIntegralSec, maxIntegralSec);
-
-      double velocityErrorDerivativeRotPerSecSq =
-          -(currentRotPerSec - previousRotPerSec) / kLoopPeriodSecs;
-      previousRotPerSec = currentRotPerSec;
-
-      double staticFf = Math.signum(targetRotPerSec) * s;
-      double velocityFf = v * targetRotPerSec;
-      double pidOutput =
-          p * errorRotPerSec + i * integralRotSec + d * velocityErrorDerivativeRotPerSecSq;
-      appliedVolts = MathUtil.clamp(staticFf + velocityFf + pidOutput, -kMaxVoltage, kMaxVoltage);
-      motorSim.setInputVoltage(MathUtil.clamp(appliedVolts, -12.0, 12.0));
+      targetVelocityRadsPerSec = 0.0;
     }
-    motorSim.update(kLoopPeriodSecs);
+    double currentVelocityRadsPerSec = slewRateLimiter.calculate(targetVelocityRadsPerSec);
 
     inputs.motorConnected = true;
-    inputs.velocityRadsPerSec = motorSim.getAngularVelocityRadPerSec() / kGearRatio;
-    inputs.appliedVolts = appliedVolts;
-    inputs.supplyCurrentAmps = motorSim.getCurrentDrawAmps();
+    inputs.velocityRadsPerSec = currentVelocityRadsPerSec;
+    inputs.appliedVolts = 0.0;
+    inputs.supplyCurrentAmps = 0.0;
   } // End updateInputs
 
   @Override
